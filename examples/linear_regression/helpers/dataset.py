41bf140aa35dd9d817e6460ba8ca0db7a6e6dacb
cecinuga <cecinuga.tdb@gmail.com>
2026-07-30 21:13:30 +0200

    fixed imports of split_dataset

--- examples/linear_regression/helpers/dataset.py
+++ examples/linear_regression/helpers/dataset.py
@@ -1,6 +1,30 @@
 from typing import Any
 import numpy as np
 
+from core.dataset.dataset import DataLoader, TensorDataset
+from core.tensor import Tensor
+
+def preprocess_dataloader(sample_tr: np.ndarray, sample_te: np.ndarray, batch_size:int = 1) -> tuple[DataLoader, DataLoader]:
+    """Helper to create a DataLoader starting to Numpy arrays"""
+
+    sample_tr_x, sample_tr_y = sample_tr[:, :-1], sample_tr[:, -1].reshape(-1, 1)
+    sample_te_x, sample_te_y = sample_te[:, :-1], sample_te[:, -1].reshape(-1, 1)
+
+    data_tr = TensorDataset(Tensor(sample_tr_x), Tensor(sample_tr_y))
+    data_te = TensorDataset(Tensor(sample_te_x), Tensor(sample_te_y))
+
+    return DataLoader(data_tr, batch_size), DataLoader(data_te, batch_size)
+
+def split_dataset(X:np.ndarray, split_ratio:float = 0.9, axis:int=0) -> tuple[np.ndarray, np.ndarray]:
+    """Split in train and test dataset"""
+
+    assert axis < len(X.shape)
+    split_idx = int(X.shape[axis]*split_ratio)
+    
+    X0, X1 = X[:, :split_idx], X[:, split_idx:]
+
+    return X0, X1
+
 def generate_noise_dataset(f: Any, size: int, sample_size: int, noise: float, min: float, max: float, seed: float) -> np.ndarray:
     rng = np.random.default_rng(seed)
     x = rng.uniform(min, max, sample_size)
--- examples/linear_regression/helpers/train.py
+++ examples/linear_regression/helpers/train.py
@@ -1,42 +1,21 @@
 import numpy as np
-from core.dataset.dataset import DataLoader, TensorDataset
+from core.dataset.dataset import DataLoader
 from core.layers import Linear, Sequential
 from core.losses import MSELoss
 from core.optimizer import SGD
-from core.tensor import Tensor
 from core.training.schedulers import CosineSchedule
 from core.training.trainer import Trainer
+from examples.linear_regression.helpers.dataset import preprocess_dataloader
 
 
-def preprocess_dataloader(sample_tr: np.ndarray, sample_te: np.ndarray, batch_size:int = 1) -> tuple[DataLoader, DataLoader]:
-    """Helper to create a DataLoader starting to Numpy arrays"""
-
-    sample_tr_x, sample_tr_y = sample_tr[:, :-1], sample_tr[:, -1].reshape(-1, 1)
-    sample_te_x, sample_te_y = sample_te[:, :-1], sample_te[:, -1].reshape(-1, 1)
-
-    data_tr = TensorDataset(Tensor(sample_tr_x), Tensor(sample_tr_y))
-    data_te = TensorDataset(Tensor(sample_te_x), Tensor(sample_te_y))
-
-    return DataLoader(data_tr, batch_size), DataLoader(data_te, batch_size)
-
-def split_dataset(X:np.ndarray, split_ratio:float = 0.9, axis:int=0) -> tuple[np.ndarray, np.ndarray]:
-    """Split in train and test dataset"""
-
-    assert axis < len(X.shape)
-    split_idx = int(X.shape[axis]*split_ratio)
-    
-    X0, X1 = X[:, :split_idx], X[:, split_idx:]
-
-    return X0, X1
-
-def create_trainer(in_feature: int, out_feature:int, min_lr: float, max_lr: float, epochs: int, weights_decay:float = 0, bias_decay:float = 0) -> Trainer:
+def create_trainer(in_feature: int, out_feature:int, min_lr: float, max_lr: float, epochs: int, weights_decay:float = 0) -> Trainer:
     """Helper to build model, loss, optimizer, scheduler and finally return the trainer"""
 
     model = Sequential(
         Linear(in_feature, out_feature)
     )
     loss = MSELoss()
-    optimizer = SGD(model.parameters, max_lr, weights_decay, bias_decay)
+    optimizer = SGD(model.parameters, max_lr, weights_decay)
     scheduler = CosineSchedule(max_lr, min_lr, epochs)
     trainer = Trainer(model, loss, optimizer, scheduler)
 
